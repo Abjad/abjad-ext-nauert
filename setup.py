b6a27fa1f343d6d4f8084216bf1266157f7bf7fd
nivlekp <osamupang@gmail.com>
2022-02-07 23:54:13 +1100

    bump abjad and abjad-ext-nauert version to 3.5

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         description=description,
         include_package_data=True,
         install_requires=[
-            "abjad==3.4",
+            "abjad==3.5",
             "black>=20.8b1",
             "flake8",
             "isort",
